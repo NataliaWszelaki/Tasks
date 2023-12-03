f8830c6c5650f8b7101b049187a36051837a435f
NataliaWszelaki <126349718+NataliaWszelaki@users.noreply.github.com>
2023-12-03 22:10:58 +0100

    23.3 @Builder - Lombok

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,46 +1,16 @@
 package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class Mail {
     private final String mailTo;
     private final String toCc;
     private final String subject;
     private final String message;
-
-    public static class MailBuilder {
-
-        private String mailTo;
-        private String toCc;
-        private String subject;
-        private String message;
-
-        public MailBuilder mailTo(String mailTo) {
-            this.mailTo = mailTo;
-            return this;
-        }
-
-        public MailBuilder toCc(String toCc) {
-            this.toCc = toCc;
-            return this;
-        }
-
-        public MailBuilder subject(String subject) {
-            this.subject = subject;
-            return this;
-        }
-
-        public MailBuilder message(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public Mail build() {
-            return new Mail(mailTo, toCc, subject, message);
-        }
-    }
 }
